456c74c67fcaddac68c84db197bfa78fc1f2e6f9
NastyLeoc <paincompiler@gmail.com>
2014-11-08 01:52:16 +0800

    Initial Commit

---
+++ paincompiler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  paincompiler
+//
+//  Created by Leoc O'Cear on 11/8/14.
+//  Copyright (c) 2014 Leoc O'Cear. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
